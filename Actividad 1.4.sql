03751f89adf84c60cc6e7c51afff260729f29113
cecilia-galarza <81053524+cecilia-galarza@users.noreply.github.com>
2021-04-04 20:16:13 -0300

    Add files via upload

--- Actividad 1.4.sql
+++ Actividad 1.4.sql
@@ -8,12 +8,12 @@ create table TiposClientes(
 )
 GO
 create table Clientes(
-    IdCliente int not null primary key identity(1,1), 
-    RazonSocial varchar(100) not null unique, 
+	IdCliente int not null primary key identity(1,1), 
+	RazonSocial varchar(100) not null unique, 
 	Cuit varchar(11) not null unique, 
 	IdTipo int not null foreign key references TiposClientes (IdTipo), 
 	Email varchar(100) null, 
-    Telefono varchar(20) null, 
+	Telefono varchar(20) null, 
 	Celular varchar(20) null,
 	Idciudad int null foreign key references Ciudades(IdCiudad)
 	)
@@ -31,9 +31,9 @@ create table Proyectos(
 
 GO
 create table Modulos(
-    IdMod int not null primary key identity(1,1), 
+	IdMod int not null primary key identity(1,1), 
 	IdProyect varchar(5) not null foreign key references Proyectos (IdProyect),
-    Nombres varchar(100) not null, 
+	Nombres varchar(100) not null, 
 	Descripcion varchar(300) null, 
 	Horas int not null check (Horas >0 ),
 	Costomod money not null check (Costomod > 0),
@@ -43,22 +43,22 @@ create table Modulos(
 	)
 Go
 create table Ciudades(
-    IdCiudad int not null primary key identity(1,1), 
-    Nombre varchar(100) not null
+	IdCiudad int not null primary key identity(1,1), 
+	Nombre varchar(100) not null
 	)
 Go
 create table Paises(
-    IdPais int not null primary key identity(1,1), 
-    Nombre varchar(100) not null
+	IdPais int not null primary key identity(1,1), 
+	Nombre varchar(100) not null
 	)
 GO
 create table Colaboradores(
-    IdColab int not null primary key identity(1,1), 
-    Apellidos varchar(100) not null, 
+	IdColab int not null primary key identity(1,1), 
+	Apellidos varchar(100) not null, 
 	Nombres varchar(100) not null,
 	Genero char(1) null check (Genero='F' or Genero='M' or Genero='N'),
 	Email varchar(100) null, 
-    Celular varchar(20) null,
+	Celular varchar(20) null,
 	check (Email != NULL or Celular !=NULL),
 	FechaNac date not null check (FechaNac < getdate()),
 	Direccion varchar(100) null,
@@ -66,11 +66,3 @@ create table Colaboradores(
 	IdPais int null foreign key references Paises(IdPais),
 	TipoColaborador char(1) not null check(TipoColaborador='I' or TipoColaborador='E')
 	)
-
-	
-	
-
-
-
-
-
